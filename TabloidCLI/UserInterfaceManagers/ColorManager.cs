eda8e1eaefae6e3f37cadcca84595bc23b44a86f
Brett Stoudt <bstoudt01@gmail.com>
2020-09-11 11:53:07 -0500

    completed background functionality with 5 options

---
+++ TabloidCLI/UserInterfaceManagers/ColorManager.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TabloidCLI.UserInterfaceManagers
+{
+    class ColorManager : IUserInterfaceManager
+    {
+        private readonly IUserInterfaceManager _parentUI;
+
+
+        public ColorManager(IUserInterfaceManager parentUI)
+        {
+            _parentUI = parentUI;
+        }
+        public IUserInterfaceManager Execute()
+        {
+            Console.WriteLine("Color Menu");
+            Console.WriteLine(" 1) DarkMagenta / White 'Miss Piggys Favorit`");
+            Console.WriteLine(" 2) White / Black");
+            Console.WriteLine(" 3) Grey / Black");
+            Console.WriteLine(" 4) Cyan / Black");
+            Console.WriteLine(" 5) White / Dark Red");
+            Console.WriteLine(" 0) Go Back");
+
+            Console.Write("> ");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    Console.BackgroundColor = ConsoleColor.DarkMagenta;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Clear();
+                    return Execute();
+
+                case "2":
+                    Console.BackgroundColor = ConsoleColor.White;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.Clear();
+                    return Execute();
+                case "3":
+                    Console.BackgroundColor = ConsoleColor.Gray;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.Clear();
+                    return Execute();
+                case "4":
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.Clear();
+                    return Execute();
+                case "5":
+                    Console.BackgroundColor = ConsoleColor.White;
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.Clear();
+                    return Execute();
+
+                case "0":
+                    return _parentUI;
+                default:
+                    Console.WriteLine("Invalid Selection");
+                    return this;
+            }
+        }
+
+    }
+}
--- TabloidCLI/UserInterfaceManagers/MainMenuManager.cs
+++ TabloidCLI/UserInterfaceManagers/MainMenuManager.cs
@@ -18,6 +18,7 @@ namespace TabloidCLI.UserInterfaceManagers
             Console.WriteLine(" 4) Post Management");
             Console.WriteLine(" 5) Tag Management");
             Console.WriteLine(" 6) Search by Tag");
+            Console.WriteLine(" 9) Change Color");
             Console.WriteLine(" 0) Exit");
 
             Console.Write("> ");
@@ -30,6 +31,9 @@ namespace TabloidCLI.UserInterfaceManagers
                 case "4": return new PostManager(this, CONNECTION_STRING);
                 case "5": return new TagManager(this, CONNECTION_STRING);
                 case "6": return new SearchManager(this, CONNECTION_STRING);
+                case "9": return new ColorManager(this);
+
+
                 case "0":
                     Console.WriteLine("Good bye");
                     return null;
