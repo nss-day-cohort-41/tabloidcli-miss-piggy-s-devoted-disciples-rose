3c5dc24c977b0d6837f1f8590db98887e5cbaa1e
Tasha Lane <designtmg77@gmail.com>
2020-09-10 21:14:33 -0500

    TagManager able to add tag

--- TabloidCLI/UserInterfaceManagers/TagManager.cs
+++ TabloidCLI/UserInterfaceManagers/TagManager.cs
@@ -1,14 +1,22 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using TabloidCLI.Models;
+/// <summary>
+/// Author of TagManager TLane
+/// </summary>
 namespace TabloidCLI.UserInterfaceManagers
 {
     public class TagManager : IUserInterfaceManager
     {
         private readonly IUserInterfaceManager _parentUI;
+        private TagRepository _tagRepository;
+        private string _connectionString;
 
         public TagManager(IUserInterfaceManager parentUI, string connectionString)
         {
             _parentUI = parentUI;
+            _tagRepository = new TagRepository(connectionString);
+            _connectionString = connectionString;
         }
 
         public IUserInterfaceManager Execute()
@@ -44,35 +52,101 @@ namespace TabloidCLI.UserInterfaceManagers
             }
         }
 
-        private void List()
+        private Tag Choose(string prompt = null)
         {
-            throw new NotImplementedException();
+            if (prompt == null)
+            {
+                prompt = "Please choose a Tag:";
+            }
+
+            Console.WriteLine(prompt);
+
+            List<Tag> tags = _tagRepository.GetAll();
+
+            for (int i = 0; i < tags.Count; i++)
+            {
+                Tag tag = tags[i];
+                Console.WriteLine($" {i + 1}) {tag.Name}");
+            }
+            Console.Write("> ");
+
+            string input = Console.ReadLine();
+            try
+            {
+                int choice = int.Parse(input);
+                return tags[choice - 1];
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Invalid Selection");
+                return null;
+            }
         }
 
-        /*private void Add()
+        private void List()
         {
-            Console.WriteLine("New Tag");
-            Tag tag = new Tag();
 
-            Console.Write("Id: ");
-            tag.Id = Console.ReadLine();
+            List<Tag> allTags = _tagRepository.GetAll();
+            Console.WriteLine($"TagID\tTag Name");
+            foreach (Tag tag in allTags)
+            {
+                Console.WriteLine($"{tag.Id}\t{tag.Name}");
+            }
 
-            Console.Write("Name: ");
-            tag.Name = Console.ReadLine();
+        }
 
-            
+        
 
-            _tagRepository.Insert(tag);
-        }*/
+
+        private void Add()
+        {
+            Console.WriteLine("Please add a Tag");
+            Console.WriteLine("Tag Name?  (ENTER to cancel)");
+
+            string tagName = Console.ReadLine();
+            if (tagName != "")
+            {
+                Tag tag = new Tag { Name = tagName };
+                _tagRepository.Insert(tag);
+                Console.WriteLine("-----------------------------");
+
+                Console.WriteLine($"Added the new Tag with id {tag.Id}");
+            }
+
+        }
 
         private void Edit()
         {
-            throw new NotImplementedException();
+            Tag tagToEdit = Choose("Which tag would you like to edit?");
+            if (tagToEdit == null)
+            {
+                return;
+            }
+
+            Console.WriteLine();
+            Console.Write("New name (blank to leave unchanged: ");
+            string name = Console.ReadLine();
+            if (!string.IsNullOrWhiteSpace(name))
+            {
+                tagToEdit.Name = name;
+            }
+
+
+            _tagRepository.Update(tagToEdit);
+
+            Console.WriteLine("Updated Tag List: ");
+            List();
         }
 
         private void Remove()
         {
-            throw new NotImplementedException();
+            Tag tagToDelete = Choose("Which tag would you like to remove?");
+            if (tagToDelete != null)
+            {
+                _tagRepository.Delete(tagToDelete.Id);//Delete Id
+            }
+            Console.WriteLine($"{tagToDelete.Name} has been deleted.");//Delete Name
+            List();
         }
     }
 }
